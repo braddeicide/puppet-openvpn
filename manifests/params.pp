d0a98e8cec83c6fd9f90725f05ec92bc8dfee626
Zach Leslie <zach@puppetlabs.com>
2013-12-14 23:54:23 -0800

    begin to manage openbsd with a bit more grace

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,12 +6,19 @@ class openvpn::params {
 
   case $::operatingsystem {
     'freebsd': {
-      $openvpn_dir  = '/usr/local/etc/openvpn'
-      $package_name = 'security/openvpn'
+      $openvpn_dir    = '/usr/local/etc/openvpn'
+      $package_name   = 'security/openvpn'
+      $manage_service = true
+    }
+    'openbsd': {
+      $openvpn_dir    = '/usr/local/etc/openvpn'
+      $package_name   = 'security/openvpn'
+      $manage_service = false
     }
     default: {
-      $openvpn_dir  = '/etc/openvpn'
-      $package_name = 'openvpn'
+      $openvpn_dir    = '/etc/openvpn'
+      $package_name   = 'openvpn'
+      $manage_service = true
     }
   }
 
